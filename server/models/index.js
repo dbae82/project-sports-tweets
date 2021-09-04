eecd3d64e35baa932ae0a337eb40ebb5fe1c5901
dbae82 <dbae82@gmail.com>
2021-09-04 16:21:38 -0500

    build: server side database connection to atlas

---
+++ server/models/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectionString = process.env.MONGODB_URI;
+
+const configOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+};
+
+mongoose.connect(connectionString, configOptions)
+    .then(() => console.log("MongoDB successfully connected..."))
+    .catch((error) => console.log(`MongoDB connection error: ${error}`));
+
+module.exports = {};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 
+const PORT = process.env.PORT;
 const app = express();
-
-const PORT = 4000;
 
 app.get('/', (req, res) => {
     res.send(`Welcome to Port ${PORT}!!`);
