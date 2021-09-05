3fc57758a6571681b2fa28fb864d3b3c1793d0e3
dbae82 <dbae82@gmail.com>
2021-09-05 18:55:12 -0500

    edit: home page with a gif on splash card

--- react-sportstweets/src/components/Forms/RegisterForm.jsx
+++ react-sportstweets/src/components/Forms/RegisterForm.jsx
@@ -2,21 +2,23 @@ import { Form, Input, Button } from "semantic-ui-react";
 
 const RegisterForm = () => {
   return (
-    <Form>
-      <Form.Field>
-        <label>Username</label>
-        <Input placeholder="Username" />
-      </Form.Field>
-      <Form.Field>
-        <label>Email</label>
-        <Input placeholder="example@email.com" />
-      </Form.Field>
-      <Form.Field>
-        <label>Password</label>
-        <Input placeholder="Password" />
-      </Form.Field>
-      <Button type='submit'>Submit</Button>
-    </Form>
+    <div>
+      <Form>
+        <Form.Field>
+          <label>Username</label>
+          <Input placeholder="Username" />
+        </Form.Field>
+        <Form.Field>
+          <label>Email</label>
+          <Input placeholder="example@email.com" />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <Input placeholder="Password" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </div>
   );
 };
 
--- react-sportstweets/src/pages/Home.jsx
+++ react-sportstweets/src/pages/Home.jsx
@@ -1,17 +1,25 @@
-import { Container } from "semantic-ui-react";
+import { Container, Image, Grid } from "semantic-ui-react";
 import RegisterForm from "../components/Forms/RegisterForm";
 
 import background from "../assets/nba-teams.png";
+import splashGif from "../assets/splash-card.gif";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home-container__background">
-        <img src={background} alt="nba-teams" />
+        <img src={background} alt="nba-teams" id="background-image" />
       </div>
       <Container className="home-container__card">
-        <RegisterForm />
+        <Grid columns={2}>
+          <Grid.Column>
+            <Image src={splashGif} />
+          </Grid.Column>
+          <Grid.Column>
+            <RegisterForm />
+          </Grid.Column>
+        </Grid>
       </Container>
     </div>
   );
