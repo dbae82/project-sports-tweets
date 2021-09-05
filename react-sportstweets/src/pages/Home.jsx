83b915ecc1dd97c3e94d5a9caf8d8b553910b37f
dbae82 <dbae82@gmail.com>
2021-09-05 13:28:57 -0500

    build: register and login components

--- react-sportstweets/src/pages/Home.jsx
+++ react-sportstweets/src/pages/Home.jsx
@@ -1,9 +1,18 @@
+import { Container } from "semantic-ui-react";
+import RegisterForm from "../components/Forms/RegisterForm";
+
+import background from "../assets/nba-teams.png";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
-        
+      <div className="home-container__background">
+        <img src={background} alt="nba-teams" />
+      </div>
+      <Container className="home-container__card">
+        <RegisterForm />
+      </Container>
     </div>
   );
 };
