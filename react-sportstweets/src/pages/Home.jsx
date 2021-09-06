d1234ab5348f41ff24da9a52186c85b737e513d0
dbae82 <dbae82@gmail.com>
2021-09-05 19:19:30 -0500

    edit: home page with dummy content to fill home container card

--- react-sportstweets/src/pages/Home.jsx
+++ react-sportstweets/src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import { Container, Image, Grid } from "semantic-ui-react";
-import RegisterForm from "../components/Forms/RegisterForm";
+import { Container, Image, Grid, Divider, Header } from "semantic-ui-react";
 
 import background from "../assets/nba-teams.png";
 import splashGif from "../assets/splash-card.gif";
@@ -12,12 +11,27 @@ const Home = () => {
         <img src={background} alt="nba-teams" id="background-image" />
       </div>
       <Container className="home-container__card">
-        <Grid columns={2}>
-          <Grid.Column>
+        <Grid celled>
+          <Grid.Column width={10}>
             <Image src={splashGif} />
           </Grid.Column>
-          <Grid.Column>
-            <RegisterForm />
+          <Grid.Column width={6}>
+            <Container fluid>
+              <Header as='h3' textAlign='center'>Welcome to Sports Tweets</Header>
+              <Divider />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae eum amet totam consequuntur neque adipisci ad, quibusdam architecto dignissimos numquam enim sequi quasi, quos illo animi fugiat eveniet quo?
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea ad hic, consequuntur facere sint voluptatem sit dolorem, aliquid accusamus illo laudantium porro nihil culpa exercitationem quod ex soluta, obcaecati quibusdam!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea ad hic, consequuntur facere sint voluptatem sit dolorem, aliquid accusamus illo laudantium porro nihil culpa exercitationem quod ex soluta, obcaecati quibusdam!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea ad hic, consequuntur facere sint voluptatem sit dolorem, aliquid accusamus illo laudantium porro nihil culpa exercitationem quod ex soluta, obcaecati quibusdam!
+              </p>
+            </Container>
           </Grid.Column>
         </Grid>
       </Container>
